9d137f99bc6d9f128a1c24dd1385e105c9935ef6
New Panuwat <new_sbh@me.com>
2024-11-23 09:44:42 +0700

    Add: btrees add_child()

---
+++ tree/btree.py
@@ -0,0 +1,11 @@
+class TreeNodes:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.value)
+
+
+
